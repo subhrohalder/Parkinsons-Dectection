ac87385feb26716b9d1b4689a66fd7b3b5ff4e80
subhrohalder <subhohalder31@gmail.com>
2020-07-18 01:50:26 +0530

    initial commit
    initial commit

---
+++ parkinson_program.py
@@ -0,0 +1,35 @@
+#import library
+import numpy as np
+import pandas as pd
+
+#import dataset
+dataset = pd.read_csv('parkinsons.csv')
+
+#data selection
+y=dataset.loc[:,'status'].values
+X=dataset.drop(columns=['name','status']).values
+
+#Split Train and Test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.25)
+
+#Standardization(Feature Scaling)
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+
+sc.fit(X_train)
+X_train=sc.transform(X_train)
+X_test=sc.transform(X_test)
+
+#Model Fitting
+from sklearn.linear_model import LogisticRegression
+model=LogisticRegression()
+model.fit(X_train,y_train)
+
+#Prediction
+y_pred= model.predict(X_test)
+
+#accuracy
+from sklearn.metrics import accuracy_score
+accuracy=accuracy_score(y_test,y_pred)
+
